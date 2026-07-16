06338b3f7399d9e86e65cf5a0b4a6b81f1268131
Ayesha Kanwal <kanwalayesha676@gmail.com>
2026-07-16 01:13:01 -0700

    version1

---
+++ cpp_practice1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string name;
+    cout<<"Enter your name :";
+    getline(cin,name);
+    cout<<"The name of student is :"<<name;
+    return 0;
+
+}
